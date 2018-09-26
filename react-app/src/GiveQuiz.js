eb8bb28ab76c5ffbad84dd99e0b5ff5a786f45c5
Manan Goel <manan.goel@research.iiit.ac.in>
2018-09-26 22:43:09 +0530

    LifeLines added

--- react-app/src/GiveQuiz.js
+++ react-app/src/GiveQuiz.js
@@ -9,14 +9,16 @@ class GiveQuiz extends Component {
 
         this.state = {
             data : [],
-            score : 0
+            score : 0,
+            lifelineused : false
         };
     }
 
     componentWillReceiveProps() {
         this.setState({
             data : this.props.quizData,
-            score : 0
+            score : 0,
+            lifelineused : false
         });
 
         let lis = document.getElementsByClassName("checkboxes");
@@ -71,9 +73,116 @@ class GiveQuiz extends Component {
         window.location.reload();
     }
 
+    UseExpert = (e) => {
+        if (this.state.lifelineused === false) {
+
+            let question = {};
+
+            for (var i = 0; i < this.state.data.length; i++) {
+                if (this.state.data[i].id == e.target.id) {
+                    question = this.state.data[i];
+                }
+            }
+            //console.log(question);
+
+            let correctans = 0;
+
+            if (question.option1ans === true) {
+                correctans = 1;
+            }
+            else if (question.option2ans === true) {
+                correctans = 2;
+            }
+            else if (question.option3ans === true) {
+                correctans = 3;
+            }
+            else if (question.option4ans === true) {
+                correctans = 4;
+            }
+
+            document.getElementById('opt' + correctans + '_' + e.target.id).click();
+            
+            this.setState({
+                lifelineused : true,
+                score : this.state.score - 0.5
+            });
+
+        }
+        else {
+            alert("Life Line Used");
+        }
+    }
+
+    handle50 = (e) => {
+        let id = e.target.id.replace('50_', '')
+
+        if(this.state.lifelineused === false) {
+            let question = {};
+
+            for (var i = 0; i < this.state.data.length; i++) {
+                if (this.state.data[i].id == id) {
+                    question = this.state.data[i];
+                }
+            }
+            //console.log(question);
+            if ((question.option1ans === true 
+                && question.option2ans === false 
+                && question.option3ans === false 
+                && question.option4ans === false)) {
+                    document.getElementById("opt" + 2 + "_" + id).style.display = "none";
+                    document.getElementById("opt" + 4 + "_" + id).style.display = "none";
+
+                    alert(question['option' + 2 + 'text'] + ' and ' + question['option' + 4 + 'text'] + " are incorrect");
+            } 
+            else if (question.option2ans === true &&
+                question.option1ans === false &&
+                question.option3ans === false &&
+                question.option4ans === false) {
+                    alert(question['option' + 1 + 'text'] + ' and ' + question['option' + 4 + 'text'] + " are incorrect");
+            }
+            else if (question.option3ans === true &&
+                question.option2ans === false &&
+                question.option1ans === false &&
+                question.option4ans === false) {
+                    alert(question['option' + 1 + 'text'] + ' and ' + question['option' + 2 + 'text'] + " are incorrect");
+            }
+
+            else if (question.option4ans === true &&
+                question.option2ans === false &&
+                question.option3ans === false &&
+                question.option1ans === false) {
+                    alert(question['option' + 1 + 'text'] + ' and ' + question['option' + 3 + 'text'] + " are incorrect");
+            }
+            else {
+                alert("50 : 50 can't be used on this question but you've lost the lifeline and the score :P");
+            }
+
+            this.setState({
+                lifelineused : true,
+                score : this.state.score - 0.25
+            });
+
+        }
+        else {
+            alert("Life Line Used");
+        }
+
+    }
+
     render() {
         return (
             <div className="container">
+            <h5>
+                Expert Advice will give you one of the correct options but 0.5 will be deducted from the total score. Use Wisely!
+            </h5>
+            <br />
+            <h5>
+                50 : 50 is applicable to only questions with one option correct and will cause a deduction of 0.25 from the total score. Use Wisely!
+            </h5>
+            <br />
+            <h5>
+                Only <strong>One</strong> LifeLine can be used in the quiz
+            </h5>
                 <table className="table table-dark table-hover table-responsive">
                 <thead>
                     <tr>
@@ -82,6 +191,12 @@ class GiveQuiz extends Component {
                         </th>
                         <th>
                             Options
+                        </th>
+                        <th>
+                            Expert Advice
+                        </th>
+                        <th>
+                            50 : 50
                         </th>
                     </tr>
                 </thead>
@@ -104,6 +219,8 @@ class GiveQuiz extends Component {
                                     <Checkbox className="checkboxes" id={'opt4_' + item.id } />
                                 } label={item.option4text} />
                                 </td>
+                                <td><Button onClick={(e) => this.UseExpert(e)} variant="raised" color="primary" id={item.id}>Use Expert Advice</Button></td>
+                                <td><Button onClick={(e) => this.handle50(e)} variant="raised" color="primary" id={'50_' + item.id}>Use 50:50</Button></td>
 
                             </tr>
                         )
